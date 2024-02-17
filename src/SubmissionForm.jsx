f4dc795cf82722692f48f93963c8fe919f7a337c
Utsav Kashichhwa <utsavk0112@gmail.com>
2024-02-17 15:02:35 +1100

    Link to form to API

--- src/SubmissionForm.jsx
+++ src/SubmissionForm.jsx
@@ -3,18 +3,91 @@ import TextField from "@mui/material/TextField";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import Button from "@mui/material/Button";
+import Backdrop from "@mui/material/Backdrop";
+import CircularProgress from "@mui/material/CircularProgress";
+
 import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
 
 import Alert from "@mui/material/Alert";
 import { useState } from "react";
 
 function SubmissionForm() {
-    const [code, setCode] = useState("");
-    const [name, setName] = useState("");
-    const [rollClass, setRollClass] = useState("");
+    const [backdropOpen, setBackdropOpen] = useState(false);
+
+    const [data, setData] = useState(null);
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+
+        setFormValues({
+            ...formValues,
+            [name]: {
+                ...formValues[name],
+                value,
+            },
+        });
+    };
+
+    const submitForm = async function (event) {
+        event.preventDefault();
+
+        const newFormValues = structuredClone(formValues);
+        let shouldReturn;
+
+        await Object.keys(formValues).forEach((name) => {
+            newFormValues[name].error = formValues[name].value === "";
+        });
+
+        setFormValues(newFormValues);
+
+        await Object.keys(newFormValues).forEach((name) => {
+            if (newFormValues[name].error) {
+                console.log("ERROR");
+                shouldReturn = true;
+                return;
+            }
+        });
+
+        if (shouldReturn) {
+            return;
+        }
+
+        console.log("Making API Call");
+        setBackdropOpen(true);
+
+        setShowingMessage(true);
+        const api_endpoint = `https://tmcw-api.onrender.com/submit/?name=${formValues.name.value}&code=${formValues.code.value}&rollClass=${formValues.rollClass.value}`;
+
+        const response = await fetch(api_endpoint);
+
+        const json = await response.json();
+
+        setData(json);
+    };
+
+    const [formValues, setFormValues] = useState({
+        name: {
+            value: "",
+            error: false,
+            errorMessage: "You must enter a name",
+        },
+        rollClass: {
+            value: "",
+            error: false,
+            errorMessage: "You must enter an age",
+        },
+        code: {
+            value: "",
+            error: false,
+            errorMessage: "You must enter a code",
+        },
+    });
+
+    const [showingMessage, setShowingMessage] = useState(false);
 
     return (
-        <Paper elevation={3} sx={{ flexGrow: "1" }}>
+        <Paper elevation={4} sx={{ flexGrow: "1" }}>
             <form>
                 <List style={{ justifyContent: "center" }}>
                     <ListItem>
@@ -23,50 +96,78 @@ function SubmissionForm() {
                             answer by typing your guess, name, and class below.
                         </Alert>
                     </ListItem>
+
                     <ListItem sx={{ justifyContent: "center" }}>
                         <TextField
+                            autoComplete="off"
                             required
                             id="outlined-required"
+                            name="code"
                             label="Code"
-                            value={code}
-                            onChange={(event) => setCode(event.target.value)}
+                            error={formValues.code.error}
+                            onChange={(event) => handleChange(event)}
                         />
                     </ListItem>
                     <ListItem sx={{ justifyContent: "center" }}>
                         <TextField
+                            autoComplete="off"
                             required
                             id="outlined-required"
+                            name="name"
                             label="Your name"
-                            value={name}
-                            onChange={(event) => setName(event.target.value)}
+                            error={formValues.name.error}
+                            onChange={(event) => handleChange(event)}
                         />
                     </ListItem>
                     <ListItem sx={{ justifyContent: "center" }}>
                         <TextField
+                            autoComplete="off"
                             required
                             id="outlined-required"
+                            name="rollClass"
                             label="Your class"
-                            value={name}
-                            onChange={(event) => setName(event.target.value)}
+                            error={formValues.rollClass.error}
+                            onChange={(event) => handleChange(event)}
                         />
                     </ListItem>
                     <ListItem sx={{ justifyContent: "center" }}>
-                        <Button
-                            variant="outlined"
-                            onClick={async function () {
-                                const api_endpoint = `https://tmcw-api.onrender.com/?name=${name}&code=${code}`;
-
-                                const response = await fetch(api_endpoint);
-
-                                const json = await response.json();
-                                alert(json);
-                            }}
-                        >
+                        <Button variant="outlined" onClick={submitForm}>
                             Submit
                         </Button>
                     </ListItem>
                 </List>
             </form>
+            <Backdrop open={backdropOpen} sx={{ zIndex: "1" }}>
+                {data ? (
+                    <Paper sx={{ width: "350px" }}>
+                        <List>
+                            <ListItem>
+                                <Typography variant="h6">
+                                    {data.status}
+                                </Typography>
+                            </ListItem>
+                            <Divider></Divider>
+                            <ListItem>
+                                <Typography variant="body1">
+                                    {data.message}
+                                </Typography>
+                            </ListItem>
+                            <ListItem sx={{ justifyContent: "right" }}>
+                                <Button
+                                    onClick={() => {
+                                        setBackdropOpen(false);
+                                        setData(null);
+                                    }}
+                                >
+                                    Close
+                                </Button>
+                            </ListItem>
+                        </List>
+                    </Paper>
+                ) : (
+                    <CircularProgress />
+                )}
+            </Backdrop>
         </Paper>
     );
 }
